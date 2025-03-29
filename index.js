a7f2af2e649c27a26d3dac78599040a7871c93ac
Caioxm1 <141528308+Caioxm1@users.noreply.github.com>
2025-03-29 10:14:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -230,6 +230,8 @@ async function interpretarMensagemComOpenRouter(texto) {
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${OPENROUTER_API_KEY}`
+          'HTTP-Referer': 'https://34.27.125.225',
+          'X-Title': 'botwpp'
         }
       }
     );
@@ -285,6 +287,8 @@ async function gerarRespostaConversacao(texto) {
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${OPENROUTER_API_KEY}`
+          'HTTP-Referer': 'https://34.27.125.225',
+          'X-Title': 'botwpp'
         }
       }
     );
@@ -536,6 +540,8 @@ const { state, saveCreds } = await useMultiFileAuthState('auth_info');
     printQRInTerminal: true,
     syncFullHistory: false,
     connectTimeoutMs: 60000,
+    keepAliveIntervalMs: 30_000,
+    browser: ['Bot Financeiro', 'Chrome', '115.0.0.0'],
     shouldIgnoreJid: jid => {
       const isGrupoAutorizado = GRUPOS_PERMITIDOS.includes(jid);
       const isUsuarioAutorizado = USUARIOS_AUTORIZADOS.includes(jid);
